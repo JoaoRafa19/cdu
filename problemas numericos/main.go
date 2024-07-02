d7de4cca1ce63aac93f0ff5fafaaad662cc995e7
JoaoRafa19 <joaopedrorafael19@gmail.com>
2024-07-02 14:55:11 -0300

    Pin Pan

--- problemas numericos/main.go
+++ problemas numericos/main.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	for i:= 0; i <= 100; i++ {
+		if i%3 == 0 {
+			fmt.Println("Pin")
+		}else if i %5 == 0 {
+			fmt.Println("Pan")
+		}else{
+			fmt.Println(i);
+		}
+	}
+}
+
+func multipleThree() {
 	for i := 0; i <= 100; i++ {
 		if i%3 == 0 {
 			fmt.Printf("%d ", i)
